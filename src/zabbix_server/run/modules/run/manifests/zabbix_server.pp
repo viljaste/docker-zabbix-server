acc4f8b318a350ad48c8751bb19cfb51134313e3
Jürgen Viljaste <j.viljaste@gmail.com>
2015-07-03 08:22:55 +0300

    Update

--- src/zabbix_server/run/modules/run/manifests/zabbix_server.pp
+++ src/zabbix_server/run/modules/run/manifests/zabbix_server.pp
@@ -1,4 +1,4 @@
-class run::zabbix_server_mysql {
+class run::zabbix_server {
   file { '/etc/zabbix/zabbix_server.conf':
     ensure => present,
     content => template('run/zabbix_server.conf.erb'),
@@ -12,14 +12,17 @@ class run::zabbix_server_mysql {
   }
 
   bash_exec { "cat /usr/share/zabbix-server-mysql/schema.sql | mysql -h '$mysqld_host' -P '$mysqld_port' -uroot -proot zabbix":
+    timeout => 0,
     require => Bash_exec["mysql -h '$mysqld_host' -P '$mysqld_port' -uroot -proot -e 'CREATE DATABASE zabbix;'"]
   }
 
   bash_exec { "cat /usr/share/zabbix-server-mysql/images.sql | mysql -h '$mysqld_host' -P '$mysqld_port' -uroot -proot zabbix":
+    timeout => 0,
     require => Bash_exec["cat /usr/share/zabbix-server-mysql/schema.sql | mysql -h '$mysqld_host' -P '$mysqld_port' -uroot -proot zabbix"]
   }
 
   bash_exec { "cat /usr/share/zabbix-server-mysql/data.sql | mysql -h '$mysqld_host' -P '$mysqld_port' -uroot -proot zabbix":
+    timeout => 0,
     require => Bash_exec["cat /usr/share/zabbix-server-mysql/images.sql | mysql -h '$mysqld_host' -P '$mysqld_port' -uroot -proot zabbix"]
   }
 }
